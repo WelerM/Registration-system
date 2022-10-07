6b6c0a253cf54096f73306f48d75c553385c698a
Weler Martins <welerson25@yahoo.com>
2022-10-07 18:07:34 -0300

    progress

--- index.js
+++ index.js
@@ -2,62 +2,174 @@ const { response } = require('express')
 const express = require('express')
 const Datastore = require('nedb')
 const app = express()
-app.listen(3005, () => console.log('listening at 3000'))
+app.listen(4000, () => console.log('http://localhost:4000/'))
 app.use(express.static('public'))//Links with index.html
 app.use(express.json({ limit: '1mb' }))
-
-
 const database = new Datastore('database.db')
 database.loadDatabase()
 
-/* const data = {
-    mês: 'janeiro', dia: [
-        [//01
-            {
-                name: 'joni',
-                cpf: '040.488.480.09',
-                andar: '5°',
-                hora: '12:25',
-                data: '02/09/2022'
-            }
-      
-        ]
-    ]
-}
- */
-//database.insert()
-
-
-
-
-
-app.get('/api', (req, res) => {
-    database.find({}, (err, data) => {
+//Data query
+app.get('/janeiro', (req, res) => {
+    database.find({ mês: "janeiro" }, (err, data) => {
         if (err) {
-            response.end()
+            res.end()
             return
         }
-        res.json(data)//sents data to frontend
+        res.json(data)
+    })
+})
 
+app.get('/fevereiro', (req, res) => {
+    database.find({ mês: "fevereiro" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
 
+app.get('/marco', (req, res) => {
+    database.find({ mês: "março" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
 
-        /*         const data_1 = JSON.stringify(data)
-             clear   const data_2 = JSON.parse(data_1)
-                res.json(data_2[0].name) */
+app.get('/abril', (req, res) => {
+    database.find({ mês: "abril" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/maio', (req, res) => {
+    database.find({ mês: "maio" }, (err, data) => {
+    if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/junho', (req, res) => {
+    database.find({ mês: "junho" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/julho', (req, res) => {
+    database.find({ mês: "julho" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/agosto', (req, res) => {
+    database.find({ mês: "agosto" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/setembro', (req, res) => {
+    database.find({ mês: "setembro" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/outubro', (req, res) => {
+    database.find({ mês: "outubro" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/novembro', (req, res) => {
+    database.find({ mês: "novembro" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
+    })
+})
+
+app.get('/dezembro', (req, res) => {
+    database.find({ mês: "dezembro" }, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)
     })
 })
 
 
 
 
+
+
+
+
+
+app.get('/dia', (req, res) => {
+    //Filtered data to be sent to front end
+    database.find({}, (err, data) => {
+        if (err) {
+            res.end()
+            return
+        }
+        res.json(data)//sents data to frontend
+    })
+})
+
+
+//===============================================
+//===============================================
+//===============================================
+
+
+//Data entry
 app.post('/api', (req, res) => {
     const data = req.body
-
     // console.log(req.body);
-    res.json({})
-   console.log(data);
-
+    res.json(data)//Not using it results in Uncaught (in promise) TypeError: NetworkError when attempting to fetch resource.  
+    //console.log(data);//Logs in SERVIDOR
     database.insert(data)
 
-
 })
+
+
+
+
+
+
+
+
+
+
